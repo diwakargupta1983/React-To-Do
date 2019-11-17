c7cab107fbafeb3300852df4a3a1ff6e429b760c
diwakargupta1983 <diwakargupta39@gmail.com>
2019-11-17 17:16:08 +0530

    testing commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import AddTodo from './containers/AddToDo';
 
 class App extends Component {
   state = {
-    todos: []
+    todos: [],
   }
   markToDoAsComplete = (id) => {
       this.setState({
